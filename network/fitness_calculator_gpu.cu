71ac64881423c4143c06c08d054ffc0dcae82991
Emmanuele Villa <emmanuele.villa89@gmail.com>
2023-02-16 15:53:07 +0100

    update blocks

--- network/fitness_calculator_gpu.cu
+++ network/fitness_calculator_gpu.cu
@@ -3,7 +3,6 @@
 //
 
 #include "fitness_calculator_gpu.cuh"
-#include "init_networks.cuh"
 #include "../defines.cuh"
 
 #include <stdio.h>
@@ -15,9 +14,51 @@ __global__ void calculateConvolutionGPU(
 ) {
     int imageIndex = blockIdx.x;
     int networkIndex = blockIdx.y;
-    int filterIndex = blockIdx.z;
-    int pixelIndex = threadIdx.x;
-    printf("imageIndex: %d, networkIndex: %d, filterIndex: %d", imageIndex, networkIndex, filterIndex);
+    int i = threadIdx.x + 1;
+    int j = threadIdx.y + 1;
+    int filter = threadIdx.z;
+
+    __shared__ float image[IMAGE_INPUT_SIZE];
+    __shared__ float network[NUM_WEIGHTS];
+    __shared__ float conv[CONV_IMAGE_SIZE];
+
+    // copy the image to the block shared memory
+    image[threadIdx.x * 28 + threadIdx.y] = images[imageIndex * 28 * 28 + threadIdx.x * 28 + threadIdx.y];
+
+    // copy the network to the block shared memory. I need to copy only the filters (so the first 45 weights)
+    int weightIndex = blockIdx.x;
+    if (weightIndex < 45) {
+        network[weightIndex] = networks[networkIndex * NUM_WEIGHTS + weightIndex];
+    }
+
+    __syncthreads();
+
+    float sum = 0;
+
+    // Split this for in multiple threads with threadId.z? Or loop unrolling?
+    int start = filter * 9;
+    int i_1 = (i - 1) * 28;
+    int j_1 = j - 1;
+    int i_2 = i * 28;
+    int i_3 = (i + 1) * 28;
+    int j_3 = j + 1;
+    sum += image[i_1 + j_1] * network[start];
+    sum += image[i_1 + j] * network[start + 1];
+    sum += image[i_1 + j_3] * network[start + 2];
+
+    sum += image[i_2 + j_1] * network[start + 3];
+    sum += image[i_2 + j] * network[start + 4];
+    sum += image[i_2 + j_3] * network[start + 5];
+
+    sum += image[i_3 + j_1] * network[start + 6];
+    sum += image[i_3 + j] * network[start + 7];
+    sum += image[i_3 + j_3] * network[start + 8];
+
+    int count = i * 26 + j * 26;
+
+    conv[count] = sum;
+
+    __syncthreads();
 }
 
 void calculateFitnessGPU(
@@ -32,6 +73,7 @@ void calculateFitnessGPU(
     float *d_images;
     float *d_networks;
     int *d_labels;
+
     // TODO check if pinned memory is faster
     cudaMalloc((void **) &d_images, dataCount * 28 * 28 * sizeof(float));
     cudaMalloc((void **) &d_networks, networkCount * NUM_WEIGHTS * sizeof(float));
@@ -41,5 +83,13 @@ void calculateFitnessGPU(
     cudaMemcpy(d_networks, networks, networkCount * NUM_WEIGHTS * sizeof(float), H2D);
     cudaMemcpy(d_labels, labels, dataCount * sizeof(int), H2D);
 
-    calculateConvolutionGPU<<<(dataCount, networkCount, FILTER_SIZE), 26 * 26>>>(d_images, d_networks);
+    // grid = data and network indexes
+    dim3 grid(dataCount, networkCount);
+
+    // 1 block = 1 network with 1 input image, 26x26xFILTER_SIZE threads
+    dim3 block(26, 26, FILTER_SIZE);
+
+    calculateConvolutionGPU<<<grid, block>>>(d_images, d_networks);
+
+    CHECK(cudaDeviceSynchronize());
 }
