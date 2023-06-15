e79afedce2567f02cb336de72262059c8db43c94
Batman <Batman>
2023-06-15 19:59:31 +0200

    executable

---
+++ CMakeFiles/ga_cnn.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ga_cnn.dir/args_handler/args_extractor.cu.o"
+  "CMakeFiles/ga_cnn.dir/args_handler/args_handler.cu.o"
+  "CMakeFiles/ga_cnn.dir/args_handler/args_handler_help.cu.o"
+  "CMakeFiles/ga_cnn.dir/args_handler/args_handler_version.cu.o"
+  "CMakeFiles/ga_cnn.dir/cmake_device_link.o"
+  "CMakeFiles/ga_cnn.dir/data/data_loader.cu.o"
+  "CMakeFiles/ga_cnn.dir/data/data_loader_cpu.cu.o"
+  "CMakeFiles/ga_cnn.dir/data/data_loader_gpu.cu.o"
+  "CMakeFiles/ga_cnn.dir/data/data_utils.cu.o"
+  "CMakeFiles/ga_cnn.dir/data/file_loader.cu.o"
+  "CMakeFiles/ga_cnn.dir/defines.cu.o"
+  "CMakeFiles/ga_cnn.dir/genetic/evolve_population.cu.o"
+  "CMakeFiles/ga_cnn.dir/genetic/evolve_population_cpu.cu.o"
+  "CMakeFiles/ga_cnn.dir/genetic/evolve_population_gpu.cu.o"
+  "CMakeFiles/ga_cnn.dir/info/device_info.cu.o"
+  "CMakeFiles/ga_cnn.dir/main.cu.o"
+  "CMakeFiles/ga_cnn.dir/network/fitness_calculator.cu.o"
+  "CMakeFiles/ga_cnn.dir/network/fitness_calculator_cpu.cu.o"
+  "CMakeFiles/ga_cnn.dir/network/fitness_calculator_gpu.cu.o"
+  "CMakeFiles/ga_cnn.dir/network/init_networks.cu.o"
+  "CMakeFiles/ga_cnn.dir/network/init_networks_utils.cu.o"
+  "CMakeFiles/ga_cnn.dir/output/output_utils.cu.o"
+  "ga_cnn"
+  "ga_cnn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/ga_cnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
