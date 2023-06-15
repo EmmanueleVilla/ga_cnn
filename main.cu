1730e3e523a725600c83fe5923a7bc607c8e5675
Emmanuele <emmanuele.villa89@gmail.com>
2023-06-15 19:48:38 +0200

    removed

--- main.cu
+++ main.cu
@@ -1,13 +1,6 @@
 #include <iostream>
 #include "args_handler/args_handler.cuh"
 
-// TODO use this instead of clock
-static double cpuSecond() {
-    struct timeval tp;
-    gettimeofday(&tp, NULL);
-    return ((double)tp.tv_sec + (double )tp.tv_usec*1.e-6);
-}
-
 int main(int argc, char **argv) {
     cudaDeviceReset();
     if (!handle(argc, argv)) {
