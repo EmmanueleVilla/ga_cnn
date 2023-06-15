6fa24af0f80a71ae3d4d801aaf844a03cbcc0eaa
Emmanuele <emmanuele.villa89@gmail.com>
2023-06-15 19:52:05 +0200

    removed custom rand

--- genetic/evolve_population_cpu.cu
+++ genetic/evolve_population_cpu.cu
@@ -9,24 +9,11 @@
 #include <stdio.h>
 #include <random>
 
-// A function to return a seeded random number generator.
-inline std::mt19937 &generator() {
-    // the generator will only be seeded once (per thread) since it's static
-    static thread_local std::mt19937 gen(std::random_device{}());
-    return gen;
-}
-
-// A function to generate integers in the range [min, max]
-template<typename T, std::enable_if_t<std::is_integral_v<T>> * = nullptr>
-T my_rand(T min, T max) {
-    std::uniform_int_distribution<T> dist(min, max);
-    return dist(generator());
-}
 
 int tournamentSelection(const float *fitness, int popSize) {
-    int parent = my_rand(0, popSize);
+    int parent = rand() % popSize;
     for (int j = 0; j < TOURNAMENT_SIZE; j++) {
-        int candidate = my_rand(0, popSize);
+        int candidate = rand() % popSize;
         if (fitness[candidate] > fitness[parent]) {
             parent = candidate;
         }
@@ -46,9 +33,9 @@ int my_max(int a, int b) {
 float *crossover(const float *parent1, const float *parent2) {
     auto *child = new float[NUM_WEIGHTS];
 
-    int first = my_rand(0, NUM_WEIGHTS);
-    int second = my_rand(0, NUM_WEIGHTS);
-    int third = my_rand(0, NUM_WEIGHTS);
+    int first = rand() % NUM_WEIGHTS;
+    int second = rand() % NUM_WEIGHTS;
+    int third = rand() % NUM_WEIGHTS;
 
     int start = my_min(first, my_min(second, third));
     int end = my_max(first, my_max(second, third));
@@ -63,9 +50,9 @@ float *crossover(const float *parent1, const float *parent2) {
     }
 
     // random number between 0 and 100
-    int random = my_rand(0, 250);
+    int random = rand() % 250;
     for (int i = 0; i < random; i++) {
-        int index = my_rand(0, NUM_WEIGHTS);
+        int index = rand() % NUM_WEIGHTS;
         child[index] = randGaussian();
     }
     return child;
